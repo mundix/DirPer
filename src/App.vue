dd8105da03ed9a5795a96c7c32f9b639574c56cd
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-26 07:45:44 -0400

    Creando la priemra directiva personal "decorar" y modificar los atributos del texto de css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-
+  <div class="container">
+    <div class="jumbotron">
+      <span v-decorar>Aprende Vue Js 2 Facilmente</span>
+      <!-- v-decorar es la directiva  -->
+    </div>
+  </div>
 </template>
 
 <script>
+// vAmos a crear una directiva dentro del span,
+// como podemos crearlas y que vamos hacer dentro de ellas
+// Hay dos formas , globarl y local
+//en main.js de manera global se define la directiva
 export default {
 
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Directiva Global , como definirla
+Vue.directive('decorar',{
+  //segundo argumento es un objeto, ponemos el gancho "bind"
+  //el => element
+  // binding => valores o modificadores que enviemos
+  //vnode => algo que vue js usa y no se va a tocar
+  bind(el,binding,vnode) {
+    el.style.fontFamily = 'time new roman';
+    el.style.fontWeight = 'bold';
+    el.style.fontSize = '24px';
+
+  }
+})
+//Esta directiva sera llamada dentro del span , o dentro del elemeto que querrams.
+
 new Vue({
   el: '#app',
   render: h => h(App)
